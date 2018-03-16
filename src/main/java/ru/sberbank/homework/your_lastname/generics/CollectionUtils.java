c0dc561d472ccfd1a86fcf47cc41637b90c65f62
mj <madjo85@yandex.ru>
2018-03-16 16:26:34 +0300

    1 + all generics

--- src/main/java/ru/sberbank/homework/your_lastname/generics/CollectionUtils.java
+++ src/main/java/ru/sberbank/homework/your_lastname/generics/CollectionUtils.java
@@ -13,19 +13,24 @@ import java.util.stream.Collectors;
 public class CollectionUtils {
     public static <T> void addAll(List<? extends T> source, List<? super T> destination) {
         destination.addAll(source);
+        System.out.println("destination " + destination.get(0));
+        System.out.println("source " + destination.get(0));
     }
 
     public static <T> List<T> newArrayList() {
         return new ArrayList<>();
     }
 
-    public static <T> int indexOf(List<T> source, T o) {
+    public static <T> int indexOf(List<? super T> source, T o) {
         return source.indexOf(o);
     }
 
-    public static <T> List<T> limit(List<T> source, int size) {
-        return source.stream().limit(size).collect(Collectors.toList());
+    public static <T> List<T> limit(List<? extends T> source, int size) {
+        return source.stream()
+                .limit(size)
+                .collect(Collectors.toList());
     }
+
 
     public static <T> void add(List<? super T> source, T o) {
         source.add(o);
@@ -33,7 +38,7 @@ public class CollectionUtils {
 
     public static <T> void removeAll(List<? super T> removeFrom, List<? extends T> c2) {
         for (T element:c2) {
-            if (removeFrom.indexOf(element) > 0) {
+            if (removeFrom.contains(element)) {
                 removeFrom.remove(element);
             }
         }
@@ -43,37 +48,36 @@ public class CollectionUtils {
         return c1.containsAll(c2);
 
     }
-    //true если первый лист содержит хотя-бы 1 второго
-    public static boolean containsAny(List c1, List c2) {
+
+    public static <T> boolean containsAny(List<? super T> c1, List<? extends T> c2) {
+        for (T element:c2) {
+            if (c1.contains(element)) {
+                return true;
+            }
+        }
         return false;
     }
 
-//    // Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
-//    // Элементы сравнивать через Comparable.
-//    // Пример range(Arrays.asList(8,1,3,5,6, 4), 3, 6) вернет {3,4,5,6}
-//    public static List range(List list, Object min, Object max) {
-//        return null;
-//    }
+    // Возвращает лист, содержащий элементы из входного листа в диапазоне от min до max.
+    // Элементы сравнивать через Comparable.
+    // Пример range(Arrays.asList(8,1,3,5,6, 4), 3, 6) вернет {3,4,5,6}
 
-    public static <T> List<T> range(List<T> list, T min, T max) {
-        List<T> tempList = new ArrayList<>(list);
-        tempList.sort(Comparator.comparing(T::toString));
 
-        if ((!tempList.contains(min)) || (!tempList.contains(max))) {
-            return null;
-        }
-
+    public static <T extends Comparable<T>> List<T> range(List<? extends T> list, T min, T max) {
+        List<T> tmpList = new ArrayList<>(list);
         for (T element:list) {
-            if ((element.toString().compareTo(min.toString()) < 0) || (element.toString().compareTo(max.toString()) > 0)) {
-                tempList.remove(element);
+            if ((element.compareTo(min) < 0) || (element.compareTo(max) > 0)) {
+                tmpList.remove(element);
             }
-
         }
-        return tempList;
+        return tmpList;
     }
 
-    public static List range(List list, Object min, Object max, Comparator comparator) {
-        return null;
+    public static <T extends Comparable<T>> List<T> range(List<? extends T> list, T min, T max, Comparator<? super T> comparator) {
+        return list.stream()
+                .sorted()
+                .filter(elm -> comparator.compare(elm,min) >= 0)
+                .filter(elm -> comparator.compare(elm,max) <= 0)
+                .collect(Collectors.toList());
     }
-
 }
